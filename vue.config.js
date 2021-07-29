644ab7762370090f52e548a2a2c8b1eab96479f0
kimozhang <1193055507@qq.com>
2021-07-29 10:05:43 +0800

    chore: update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  lintOnSave: false,
   productionSourceMap: false,
   configureWebpack: {
     output: {
+      // https://cli.vuejs.org/guide/build-targets.html#vue-vs-js-ts-entry-files
       libraryExport: 'default'
     }
   }
