6a9e0af4710566853e2f17e5e51d27e33220a0f9
kimozhang <1193055507@qq.com>
2021-09-02 15:33:57 +0800

    ci: optimize release script

--- scripts/release.js
+++ scripts/release.js
@@ -1,11 +1,19 @@
 const execa = require('execa')
-const run = (bin, args, opts = {}) =>
-  execa(bin, args, { stdio: 'inherit', ...opts })
+const run = (bin, args) =>
+  execa(bin, args, { stdio: 'inherit' })
+const step = (msg) => console.log(msg)
 
 async function main() {
+  step('Buiding package...')
   await run('yarn', ['build'])
+
+  step('Publish package...')
   await run('yarn', ['publish', '--access', 'public'])
+
+  step('Push tag to Github...')
   await run('git', ['push' '--tags'])
+
+  step('Done')
 }
 
 main().catch(e => console.error(e))
