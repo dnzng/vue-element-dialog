ab9166dc814b8b58ad1e4447e5ab0d24be43f876
dnzng <1193055507@qq.com>
2023-08-22 23:17:37 +0800

    build: remove support for browsers

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,19 +1,9 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig([
-  {
-    entry: ['src/index.ts'],
-    format: ['esm', 'cjs'],
-    dts: true,
-    treeshake: true,
-    clean: true
-  },
-  {
-    entry: ['src/index.ts'],
-    format: ['iife'],
-    globalName: 'VueElementDialog',
-    treeshake: true,
-    clean: true,
-    minify: true
-  }
-])
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  treeshake: true,
+  clean: true
+})
